d97e959419fab75d8ba718668079019af54f35d7
Marino McGalla <mdm409psu@gmail.com>
2016-09-21 00:10:55 -0400

    added prompts for test case input

--- rover.js
+++ rover.js
@@ -1,1 +1,11 @@
-﻿
+﻿function roverProgram() {
+    //these 3 lines prompt the user for test case input
+    var input1 = prompt("Greetings, astronaut! What are the upper-right coordinates your rover will be traveling today?");
+    var input2 = prompt("Where is your rover currently located and what is its oritentation?");
+    var input3 = prompt("Great! Ready for movement instructions!");
+
+    console.log(upperCoord);
+    console.log(initCoord);
+    console.log(instruct);
+
+}
